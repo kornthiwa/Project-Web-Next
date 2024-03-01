5be183053d42979e263223c19b4d2ff01a73be2c
Kornthiwa Wirata <kornthiwa.wir@everapp.io>
2024-03-01 11:59:14 +0700

    tea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { Button, Chip, Typography } from "@mui/material";
+import { Button, Card, Chip, Typography, styled } from "@mui/material";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 
 export default function Home() {
-  const { data } = useQuery<any>({
+  const {
+    data: datatesxt,
+    isLoading,
+    isError,
+  } = useQuery<any>({
     queryKey: ["todos"],
     queryFn: async () => {
       const response = await axios.get(
@@ -14,5 +19,61 @@ export default function Home() {
       return response.data;
     },
   });
-  return <div></div>;
+
+  const onClickRowFunction = (data: any) => {
+    console.log(data);
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      headerName: "Active",
+      width: 100,
+      disableColumnMenu: true,
+      align: "center",
+      headerAlign: "center",
+      renderCell: (params: any) => {
+        const { row } = params;
+        return <>{row.name}</>;
+      },
+    },
+    {
+      field: "name",
+      headerName: "Active",
+      width: 100,
+      disableColumnMenu: true,
+      align: "center",
+      headerAlign: "center",
+      renderCell: (params: any) => {
+        const { row } = params;
+        return (
+          <>
+            <Button onClick={() => onClickRowFunction(row)}>กด</Button>
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <Card sx={{ minHeight: 100, overflow: "hidden" }}>
+        <DataGrid
+          rows={(!isLoading && datatesxt) || []}
+          columns={columns}
+          getRowId={(row) => row._id}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 },
+            },
+          }}
+          // pageSizeOptions={[10, 20, 50]}
+          checkboxSelection={false}
+          rowSelection
+          onPaginationModelChange={(e) => console.log("Page", e)}
+          disableColumnMenu
+        />
+      </Card>
+    </div>
+  );
 }
--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -1,185 +1,20 @@
 "use client";
+
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { Table } from "@/module/components/Table";
 import { Box, Button, Chip, Typography } from "@mui/material"; // import Button
 import DialogPatient from "./patient/DailogPatient";
-import { Pagination } from "@/module/components/Pagination";
-
-const TableRowCancel = ({ onClickRowFunction }: any) => {
-  return [
-    {
-      render: (data: any) => {
-        return (
-          <>
-            <Typography>{data.active}</Typography>
-          </>
-        );
-      },
-      title: (
-        <>
-          <Typography>Active</Typography>
-        </>
-      ),
-      width: 100,
-      disableColumnMenu: true,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      render: (data: any) => {
-        return (
-          <>
-            <Typography>{data._id}</Typography>
-          </>
-        );
-      },
-      title: (
-        <>
-          <Typography>ID</Typography>
-        </>
-      ),
-      disableColumnMenu: true,
-      align: "left",
-      headerAlign: "center",
-    },
-    {
-      render: (data: any) => {
-        return (
-          <>
-            <Typography>{data.name}</Typography>
-          </>
-        );
-      },
-      title: (
-        <>
-          <Typography>ชื่อ</Typography>
-        </>
-      ),
-      width: 150,
-      disableColumnMenu: true,
-      align: "left",
-      headerAlign: "center",
-    },
-    {
-      render: (data: any) => {
-        return (
-          <>
-            <Typography>{data.age} ปี</Typography>
-          </>
-        );
-      },
-      title: (
-        <>
-          <Typography>อายุ</Typography>
-        </>
-      ),
-      width: 250,
-      disableColumnMenu: true,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      render: (data: any) => {
-        return (
-          <>
-            <Typography>{data.symptoms}</Typography>
-          </>
-        );
-      },
-      title: (
-        <>
-          <Typography>อาการ</Typography>
-        </>
-      ),
-      width: 250,
-      disableColumnMenu: true,
-      align: "left",
-      headerAlign: "center",
-    },
-    {
-      render: (data: any) => {
-        return (
-          <>
-            <Typography>{data.phoneNumber}</Typography>
-          </>
-        );
-      },
-      title: (
-        <>
-          <Typography>เบอร์โทร</Typography>
-        </>
-      ),
-      type: "number",
-      width: 100,
-      disableColumnMenu: true,
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      render: (data: any) => {
-        let chipLabel = "";
-        let chipColor = "default";
-        switch (data.status) {
-          case 10:
-            chipLabel = "ยังไม่กรอกข้อมูล";
-            chipColor = "info";
-            break;
-          case 20:
-            chipLabel = "กำลังกรอกข้อมูล";
-            chipColor = "warning";
-            break;
-          case 30:
-            chipLabel = "กรอกข้อมูลสำเร็จ";
-            chipColor = "success";
-            break;
-          default:
-            chipLabel = "";
-        }
-        return <>das</>;
-      },
-      title: (
-        <>
-          <Typography>สถานะ</Typography>
-        </>
-      ),
-      width: 200,
-      align: "center",
-      sortable: false,
-      headerAlign: "center",
-      disableColumnMenu: true,
-    },
-    {
-      render: (data: any) => {
-        return (
-          <>
-            <Button
-              variant="contained"
-              onClick={() => onClickRowFunction(data)}
-            >
-              ส่งตัว
-            </Button>
-          </>
-        );
-      },
-      title: "",
-      align: "center",
-      sortable: false,
-      disableColumnMenu: true,
-      headerAlign: "center",
-    },
-  ];
-};
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(!open);
-  };
-
-  const { data } = useQuery<any>({
+  const {
+    data: datatesxt,
+    isLoading,
+    isError,
+  } = useQuery<any>({
     queryKey: ["todos"],
     queryFn: async () => {
       const response = await axios.get(
@@ -188,6 +23,11 @@ export default function Home() {
       return response.data;
     },
   });
+
+  const handleClickOpen = () => {
+    setOpen(!open);
+  };
+
   const onClickRowFunction = (data: any) => {
     console.log(data);
   };
@@ -202,27 +42,57 @@ export default function Home() {
     );
   };
 
+  const columns: GridColDef[] = [
+    {
+      field: "age",
+      headerName: "Active",
+      width: 100,
+      disableColumnMenu: true,
+      align: "center",
+      headerAlign: "center",
+      renderCell: (params: any) => {
+        const { rowIndex } = params;
+        console.log(params);
+
+        return <>{rowIndex}</>;
+      },
+    },
+    {
+      field: "name",
+      headerName: "Active",
+      width: 100,
+      disableColumnMenu: true,
+      align: "center",
+      headerAlign: "center",
+      renderCell: (params: any) => {
+        const { row } = params;
+        return <Button onClick={() => onClickRowFunction(row)}>กด</Button>;
+      },
+    },
+  ];
+
   return (
     <div>
       <Box>
         <Button variant="outlined" onClick={handleClickOpen}>
           เพิ่มผู้ป่วยเข้ารับการรักษา
         </Button>
-        <Table rows={TableRowCancel(onClickRowFunction)} data={data} />
-
-        {renderDialog()}
-      </Box>
-
-      <Box sx={{ height: "100%", padding: "21px 24px" }}>
-        <Pagination
-          data-testid={"action-permission-pagination"}
-          onChangePage={(_, pageNumber) => null}
-          onChangePageSize={(size) => null}
-          total={0}
-          pageIndex={0 - 1}
-          pageSize={10}
-          totalPages={1}
+        <DataGrid
+          rows={(!isLoading && datatesxt) || []}
+          columns={columns}
+          getRowId={(row) => row._id}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 },
+            },
+          }}
+          // pageSizeOptions={[10, 20, 50]}
+          checkboxSelection={false}
+          rowSelection
+          onPaginationModelChange={(e) => console.log("Page", e)}
+          disableColumnMenu
         />
+        {renderDialog()}
       </Box>
     </div>
   );
